e104218a6ac303fcbebf5dc2e4a0140b7d5d6d47
Qayyum <temiyores1234@gmail.com>
2026-05-30 08:32:27 +0100

    fix: login issue

--- src/login.js
+++ src/login.js
@@ -1,18 +1,3 @@
-const readline = require('readline');
-
-function waitForInput(prompt) {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    rl.question(prompt, (answer) => {
-      rl.close();
-      resolve(answer.trim());
-    });
-  });
-}
-
 async function login(page) {
   console.log('Navigating directly to signin page...');
   await page.goto('https://bc.game/login/signin', {
